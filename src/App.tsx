3d48da3d2dc8729ef5460ba16b48e9c282bd86c3
happycloudy <arion.mikola@gmail.com>
2022-11-09 01:45:11 +0300

    added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {Switch} from "react-router-dom";
 import {useRoutes} from "./hooks/useRoutes";
 import {Layout} from "antd";
+import Header from "./components/smart/Header/Header";
+import 'antd/dist/antd.css'
 
 function App() {
     const routes = useRoutes()
+
     return (
         <Layout className="App">
+            <Header/>
             <Switch>{routes}</Switch>
         </Layout>
     )
--- src/components/smart/Header/Header.tsx
+++ src/components/smart/Header/Header.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {Layout} from "antd";
+import {Layout, Menu} from "antd";
+import {Link, useLocation} from "react-router-dom";
+import routes from "../../../routes/routes";
 
-const { Header: AntHeader } = Layout;
+const {Header: AntHeader} = Layout;
 
 const Header = () => {
+    const {pathname} = useLocation()
+
     return (
         <AntHeader>
-            
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={[pathname]}
+                items={routes.map(route => ({
+                    key: route.path,
+                    label: <Link to={route.path}>{route.name}</Link>
+                })).reverse()}
+            >
+            </Menu>
         </AntHeader>
     );
 };
